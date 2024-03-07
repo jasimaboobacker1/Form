99ae6568a3e648eeb9ae35c07b5735073bc6e518
unknown <jasimaboobacker1>
2024-03-07 12:44:46 +0530

    message

---
+++ index.js
@@ -0,0 +1,33 @@
+function validateForm() {
+    let firstName = document.getElementById('firstName').value;
+    let lastName = document.getElementById('lastName').value;
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+    let phoneNumber = document.getElementById('phoneNumber').value;
+    let city = document.getElementById('city').value;
+
+    if (!firstName.trim()) {
+        alert('First Name is required');
+        return false;
+    }else if (!lastName.trim()) {
+        alert('Last Name is required');
+        return false;
+    }else if (firstName.trim() === lastName.trim()) {
+        alert('First Name and Last Name cannot be the same');
+        return false;
+    }else if (!email.trim()) {
+        alert('Email is required');
+        return false;
+    }else if (!password.trim()) {
+        alert('Password is required');
+        return false;
+    }else if (!phoneNumber.trim()) {
+        alert('Phone Number is required');
+        return false;
+    }else if (!city.trim()) {
+        alert('City is required');
+        return false;
+    }
+    else alert('Signed up successfully!');
+    return true;
+}
